b91c117ba810d888b2eda0c207adf70a8886343b
sioncaled-collab <sioncaled@gmail.com>
2025-11-24 20:01:56 -0500

    Update conexion.java

--- src/main/java/com/example/demo/model/conexion.java
+++ src/main/java/com/example/demo/model/conexion.java
@@ -5,20 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexion {
-    private static String URL = "jdbc:mysql://root:ZIfAOHZPtIPKbwbuwxXLYISilHSLNeZp@maglev.proxy.rlwy.net:39367/railway";
-        private static String USER = "railway";
-        private static String PASSWORD = "ZIfAOHZPtIPKbwbuwxXLYISilHSLNeZp";
 
-        public static Connection getConnection() {
-            Connection conexion = null;
-            try 
-            {
-                conexion = DriverManager.getConnection(URL, USER, PASSWORD);    
-            } 
-            catch (SQLException e) {
-                e.getMessage();
-            }
-            return conexion;
+    private static final String URL = "jdbc:mysql://maglev.proxy.rlwy.net:39367/railway";
+    private static final String USER = "root";
+    private static final String PASSWORD = "ZIfAOHZPtIPKbwbuwxXLYISilHSLNeZp";
+
+    public static Connection getConnection() {
+        Connection conexion = null;
+        try {
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión exitosa");
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión: " + e.getMessage());
         }
+        return conexion;
+    }
 }
-
